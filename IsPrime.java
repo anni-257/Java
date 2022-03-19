a90ee2ee6794d5e26f08463bdfe64db695eddb9b
anni-257 <taraleaniket257@gmail.com>
2022-03-19 13:41:01 +0530

    Find a number is Prime

---
+++ IsPrime.java
@@ -0,0 +1,27 @@
+import java.lang.Math;
+import java.util.Scanner;
+
+class IsPrime{
+	static boolean check(int n){
+		if(n==1){
+			return false;
+		}
+		
+		for(int i=2;i<=Math.sqrt(n);i++){
+			if(n%i==0)
+				return false;
+		}
+		
+		return true;
+	}
+	
+	public static void main(String arg[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int num=sc.nextInt();
+		
+		System.out.println(num+" is a prime number: "+check(num));
+		
+	
+	}
+}
