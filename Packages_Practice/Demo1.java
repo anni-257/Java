a83e40c7a66ac9838f7b389b58db0c3093b4d45e
anni-257 <taraleaniket257@gmail.com>
2022-04-11 22:08:06 +0530

    class for mypack1

---
+++ Packages_Practice/Demo1.java
@@ -0,0 +1,15 @@
+
+package mypack1;
+
+public class Demo1{
+
+	private int a=10;
+	protected int b=20;
+	public int c=30;
+	int d=40; // Default
+	
+	public void display(){
+		
+		System.out.println("This is Demo1 class and mypack1 package"+(a+b+c+d));
+	}
+}
