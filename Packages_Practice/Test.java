78a5046fc62b2cc1b3a7997c2be8031f91361f92
anni-257 <taraleaniket257@gmail.com>
2022-04-11 22:09:37 +0530

    Class for different package

---
+++ Packages_Practice/Test.java
@@ -0,0 +1,13 @@
+
+import mypack1.Demo1;
+
+class Test{
+
+	public static void main(String args[]){
+		Demo1 d1=new Demo1();
+		
+		d1.display();
+		System.out.println("Addition: "+(d1.a+d1.b+d1.c+d1.d));
+	
+	}
+}
