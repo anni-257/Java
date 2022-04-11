f5d647005ac3eca08ddf6f5d4d459b31a866acad
anni-257 <taraleaniket257@gmail.com>
2022-04-11 22:03:19 +0530

    Zero Error Exception

---
+++ ExceptionHandling/ZeroError.java
@@ -0,0 +1,18 @@
+
+class ZeroError{
+
+	public static void main(String args[]){
+		
+			int a=10;
+			int b=0;
+			System.out.println("Division :");
+			int c=a/b;
+			System.out.println("Division :"+c);
+			
+		
+		//catch(ArithmeticException e){
+			//System.out.println("Zero Error "+e);
+		} 
+	}
+	
+
